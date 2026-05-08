f417f034449eeb7b29e1b57393c0908576be8773
Himanshu Bhavsar <himanshurb9@gmail.com>
2026-05-08 19:39:40 +0000

    Add AWS provider and Terraform variable configuration

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  description = "AWS region for infrastructure deployment"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
