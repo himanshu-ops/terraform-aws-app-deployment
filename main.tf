e1b7f8d5a8aad620637deb6952f2a97170612c6f
Himanshu Bhavsar <himanshurb9@gmail.com>
2026-05-09 09:34:07 +0000

    Add reusable Terraform network module

--- main.tf
+++ main.tf
@@ -5,3 +5,9 @@ module "network" {
   subnet_cidr      = "10.0.1.0/24"
   availability_zone = "ap-south-1a"
 }
+
+module "security_group" {
+  source = "./modules/security-group"
+
+  vpc_id = module.network.vpc_id
+}
